c6877827c67c9bef951cb6162eb134a577194a3d
riki8245 <56740262+riki8245@users.noreply.github.com>
2025-03-06 13:35:33 +0100

    website issue deploy fixed
    index.html was inside other folder.
    
    make the build without hashing:
    
    ng build --output-path docs --base-href /my-website/ --output-hashing=none

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1087, hash: '31d6de3d42182aa539098f2a2ecc4a013097906ebc54e41d91c9dde0008f9545', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1347, hash: 'c09e08e13856d690b7aac079597ba63269c038984b89722e912ec68452901af7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 38431, hash: 'f6d2a16cbae6011b0feb9696aa3caa7dd9e536a49b6fbf180b0f7bcfbacce9cc', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 7862, hash: 'ef8cc36df56957d3728a21264b9065ebed584ed1e140bfbedceb358a31e59c42', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 9943, hash: '8cfd41ec8f664c932727d338ab97133da3febb2d66cc11fdedccd25a53615dfb', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'styles-7DYX5JOD.css': {size: 1367, hash: 'd+D7FeENMkw', text: () => import('./assets-chunks/styles-7DYX5JOD_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 1051, hash: 'ca52354339b4191d591b834bbdeded03fe1ea5a754c64e7da1a2882a2cde4d2b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1320, hash: '0a4f7ff4b1402484879438d4793d8057da3127efed0dbf5a305a349d7bc5bf31', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 38395, hash: '875452671f4404f0374fe9b0dff171074c2c37d6084394c531595c775365ca5b', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 7826, hash: 'afc97a0d9d4bf97ffa3b63c9ef3d347370520f1aeb44372479a534e7937450e4', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 9907, hash: '8b90920eadffb6f2463fd3e5eac3dba43f74bc97dac789bdce237406d330b531', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'styles.css': {size: 1367, hash: 'd+D7FeENMkw', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
   },
 };
