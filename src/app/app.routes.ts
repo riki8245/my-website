c379c8e178895ee26e28a18daf5f75bb41f248f3
riki8245 <56740262+riki8245@users.noreply.github.com>
2025-03-06 13:13:30 +0100

    Website stable version Responsive for phones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: '', redirectTo: '/about', pathMatch: 'full' }, // Default route
+];
