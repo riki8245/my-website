46baf8f9f9465436551b59c371d81227949dee65
riki8245 <56740262+riki8245@users.noreply.github.com>
2025-03-06 14:22:43 +0100

    new deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1051, hash: 'ca52354339b4191d591b834bbdeded03fe1ea5a754c64e7da1a2882a2cde4d2b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1320, hash: '0a4f7ff4b1402484879438d4793d8057da3127efed0dbf5a305a349d7bc5bf31', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'projects/index.html': {size: 38395, hash: '875452671f4404f0374fe9b0dff171074c2c37d6084394c531595c775365ca5b', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
-    'about/index.html': {size: 7826, hash: 'afc97a0d9d4bf97ffa3b63c9ef3d347370520f1aeb44372479a534e7937450e4', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 9907, hash: '8b90920eadffb6f2463fd3e5eac3dba43f74bc97dac789bdce237406d330b531', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'styles.css': {size: 1367, hash: 'd+D7FeENMkw', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 1087, hash: '093b3128b74fca48de15711ccc338d5698884055e9bc063c6afc38ff77f4e0bf', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1347, hash: '611390c26e5e10ac1d9cd6abdfd07e2b4aa82ae71421eba028fcb2271d4bc210', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'projects/index.html': {size: 38369, hash: '96de9b6c69d01b0988675e11768db5e473596cc06386f5adb7c365652b82cc03', text: () => import('./assets-chunks/projects_index_html.mjs').then(m => m.default)},
+    'about/index.html': {size: 7862, hash: 'b2d6ad712aa36de656aaf6b5c55848a610650dabea70eaf63868a81f4f56335e', text: () => import('./assets-chunks/about_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 9943, hash: 'd7c42232fa8b259c47c5480f715ad8d1fc7ac9a72e9423d4fb0e791024ed6761', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'styles-7DYX5JOD.css': {size: 1367, hash: 'd+D7FeENMkw', text: () => import('./assets-chunks/styles-7DYX5JOD_css.mjs').then(m => m.default)}
   },
 };
